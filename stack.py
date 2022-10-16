674dac24df50eece863a98da09cd1f77efb12902
wadec <wadechriestenson@gmail.com>
2022-10-16 10:43:04 -0700

    10/16/2022 - WC

---
+++ stack.py
@@ -0,0 +1,16 @@
+"""
+How to implement stack using list
+"""
+
+stack = []  # create stack
+
+# use append to add items to stack
+stack.append('A')
+stack.append('B')
+stack.append('C')
+print(stack)
+
+stack.pop()  # pop the 'last in' off the stack
+
+print(stack)
+
